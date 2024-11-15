82168bb69f1e9da1fa9c5972aedbb11cbca8ff96
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-11-15 17:13:23 +0530

    Initial

---
+++ MicrosoftAuthentication/IndividualAccount/AuthenticationApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using AuthenticationApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            SessionModel sessionModel = new SessionModel();
+            Session.Add("UserName", User.Identity.Name);
+
+            if (User.IsInRole("Admin"))
+            {
+
+                sessionModel.Role = "Admin";
+                sessionModel.Addess = "dsfdfsdfds";
+                sessionModel.Name = User.Identity.Name;
+
+                Session.Add("SessionModel", sessionModel);
+         
+
+                Session.Add("Role", "Admin");
+                ViewBag.Role = "Admin";
+                // Logic for admin users
+            }
+            else
+            {
+                Session.Add("Role", "Customer");
+                ViewBag.Role = "Customer";
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            var sessionModel = Session["SessionModel"] as SessionModel;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MicrosoftAuthentication/IndividualAccount/AuthenticationApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthenticationApp.Startup))]
+namespace AuthenticationApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
